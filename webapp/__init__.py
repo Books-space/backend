80a8512cfaf2d928471e8888cb411b8b0ead239d
Alexander Lyotov <sippie84@gmail.com>
2021-10-07 14:17:49 +0300

    removed obsolete strings from webapp init

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,18 +1,12 @@
 import os
 from flask import Flask
-from flask_restful import Api
-
-from webapp.resources.routes import BookList, SpecificBook
 from webapp.resources.models import db, Books
+from webapp.resources.routes.routes import routes
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
     db.init_app(app)
-
-    api = Api(app)
-    api.add_resource(BookList, '/api/v1/book/')
-    api.add_resource(SpecificBook, '/api/v1/book/<book_id>')
-
+    app.register_blueprint(routes, url_prefix='/api/v1/book/')
     return app
