00e1781248de5b16496e8739c0f364fe24b95bb9
Vladimir Puzakov <puzakov@tochka.com>
2021-10-10 21:07:25 +0300

    chore: disable sqlalchemy modification tracking

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -7,6 +7,7 @@ from webapp.resources.routes.routes import routes
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     app.register_blueprint(routes, url_prefix='/api/v1/book/')
     return app
