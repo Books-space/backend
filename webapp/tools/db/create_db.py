631beb207ed88db90b452f7bf168e306090f0cf7
Alexander Lyotov <sippie84@gmail.com>
2021-10-26 20:44:03 +0300

    feature/exception handling on db creation

--- webapp/tools/db/create_db.py
+++ webapp/tools/db/create_db.py
@@ -3,6 +3,10 @@ from webapp import db, create_app
 
 
 def create():
-    logger = logging.getLogger(__name__)
-    db.create_all(app=create_app())
-    logger.debug('tables created')
+    try:
+        logger = logging.getLogger(__name__)
+        db.create_all(app=create_app())
+        logger.debug('tables created')
+    except Exception as exc:
+        logger.exception('Because of following exception: %s' % exc)
+
