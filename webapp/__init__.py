66d3faf2772314741d06ae7ba4b11ee6147c0bb0
Alexander Lyotov <sippie84@gmail.com>
2021-11-05 18:10:38 +0300

    feature/db operational error handler

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -7,6 +7,10 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 
+def db_is_not_available(e):
+    return 'Database is not working now!', 500
+
+
 def create_app():
     app = Flask(__name__)
     sentry_sdk.init(
@@ -15,6 +19,7 @@ def create_app():
     )
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.register_error_handler(500, db_is_not_available)
     db.init_app(app)
     app.register_blueprint(routes, url_prefix='/api/v1/books/')
     return app
--- webapp/resources/routes/routes.py
+++ webapp/resources/routes/routes.py
@@ -2,7 +2,6 @@ from typing import Optional
 from flask import Blueprint, request, jsonify, abort, make_response
 from dataclasses import asdict
 from marshmallow import ValidationError
-from psycopg2 import OperationalError
 from webapp.resources.schemas import BookSchema
 from webapp.repositories.books import BooksRepo
 
@@ -36,13 +35,11 @@ def search():
     author: Optional[str] = request.args.get('author', type=str, default='')
     search_str: Optional[str] = request.args.get('search', type=str, default='')
     desc = order == 'desc'
-    # try:
     if search_str:
         books = repo.find_any_inclusions(desc, search_str)
     else:
         books = repo.search(desc, title, author)
     return jsonify(books)
-    # except OperationalError:
 
 
 @routes.route('', methods=['POST'])
