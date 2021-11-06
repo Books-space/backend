57c2e774cfdf9ed5e7baf37fd409b451c6f1bf3b
Alexander Lyotov <sippie84@gmail.com>
2021-11-06 15:28:02 +0300

    feature/universal exception handler for REST API

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,14 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from webapp.resources.models import db
 from webapp.resources.routes.routes import routes
-
+from sqlalchemy.exc import OperationalError
+from werkzeug.exceptions import NotFound
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 
-def db_is_not_available(e):
-    return 'Database is not working now!', 500
+def handle_error(e):
+    if isinstance(e, OperationalError):
+        msg = 'Database is not available now. Please, try again later.'
+    elif isinstance(e, NotFound):
+        msg = 'The server is unanble to process this request.'
+    else:
+        if hasattr(e, 'name'):
+            msg = e.name
+        else:
+            msg = 'Something went wrong. Please, try again later.'
+    return jsonify(msg), e.code
 
 
 def create_app():
@@ -20,7 +30,7 @@ def create_app():
     )
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.register_error_handler(500, db_is_not_available)
+    app.register_error_handler(Exception, handle_error)
     db.init_app(app)
     app.register_blueprint(routes, url_prefix='/api/v1/books/')
     return app
